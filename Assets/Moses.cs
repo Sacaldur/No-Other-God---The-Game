56423f95b96e86f0ce332b55b3c2744a82b3800b
Sacaldur <sacaldur@gmx.net>
2015-10-24 18:07:28 +0200

    added a trigger to moses to detect idols and players

---
+++ Assets/Moses.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moses : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    public void EnteredVision(Collider2D collider)
+    {
+        Debug.Log("Entered moses vision: " + collider.gameObject + " (type: " + collider.GetType().Name + ")");
+    }
+}
