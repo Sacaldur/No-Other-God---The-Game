f8d6c79b75670e51dc7458b2122b735a87929bab
Sacaldur <sacaldur@gmx.net>
2015-10-24 16:20:06 +0200

    configured the input for the ui created the game scene (content needs to be added) implemented the "start game" and "exit" button in the main menu

---
+++ Assets/ExitGameButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGameButton : MonoBehaviour
+{
+    public void Clicked()
+    {
+        Application.Quit();
+        Debug.Log("ExitButtonClicked");
+    }
+}
---
+++ Assets/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneId = 1;
+
+
+    public void Clicked()
+    {
+        Application.LoadLevel(this.sceneId);
+    }
+}
