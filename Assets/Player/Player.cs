d5f2f816a4f42c66f0a7874df322884b4da25356
Sacaldur <sacaldur@gmx.net>
2015-10-24 16:02:24 +0200

    basic player movement

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    new private Rigidbody2D rigidbody;
+    [SerializeField]
+    [Tooltip("The players id, starting at 0.\nIs used to determine the players input axes.")]
+    private int playerId = 0;
+    [SerializeField]
+    private float movementSpeed = 5;
+
+
+    void Awake()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        Vector2 movementInput = this.GetMovementInput();
+
+        this.rigidbody.velocity = movementInput * this.movementSpeed;
+    }
+
+    private Vector2 GetMovementInput()
+    {
+        return new Vector2(Input.GetAxis("Horizontal" + this.playerId), Input.GetAxis("Vertical" + this.playerId));
+    }
+}
